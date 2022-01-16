f0bb85eaf1db9429313b5a3269d4d5ec8f8ad453
Diogo Correia <me@diogotc.com>
2022-01-16 13:08:27 +0000

    Refactor reading input and allow more than 2 children

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <set>
 #include <string.h>
 #include <bits/stdc++.h>
 
@@ -8,80 +8,68 @@
 typedef struct
 {
     int self;
-    std::vector<int> parents;
-    int son[NUM_CHILD];
+    std::set<int> parents;
+    std::set<int> children;
 } node;
 
-std::vector<int> all_possible;
+std::set<int> all_possible;
 /*
 call all the parents of the node
 */
 void go_up(int current, int target, node *all_nodes);
 /*
-goes to the sons of the node and check if they are equal to the target
+goes to the children of the node and check if they are equal to the target
 */
 bool go_down(int original, int current, int target, node *all_nodes);
 
 int main()
 {
-    int vertice1, vertice2;
-    scanf("%d %d", &vertice1, &vertice2);
+    int node1_i, node2_i, num_vertices, num_edges;
+    std::cin >> node1_i >> node2_i >> num_vertices >> num_edges;
 
-    int num_vertices, num_edges;
-    scanf("%d %d", &num_vertices, &num_edges);
+    node tree[num_vertices];
 
-    node tree[num_vertices + 1];
-
-    for (int i = 0; i < num_vertices + 1; i++)
+    for (int i = 0; i < num_vertices; ++i)
     {
         node new_node;
         new_node.self = i;
-        new_node.parents = std::vector<int>();
-        new_node.son[0] = -1;
-        new_node.son[1] = -1;
+        new_node.parents = std::set<int>();
+        new_node.children = std::set<int>();
         tree[i] = new_node;
     }
 
-    int c = 0;
-
-    int start, stop;
-    while (c != EOF)
+    for (int i = 0; i < num_edges; ++i)
     {
-        scanf("%d %d", &start, &stop);
-        // more than two children
-        if (tree[start].son[0] != -1 && tree[start].son[1] != -1)
+        int start, stop;
+        std::cin >> start >> stop;
+
+        tree[start].children.insert(stop);
+        tree[stop].parents.insert(start);
+
+        if (tree[stop].parents.size() > 2)
         {
-            printf("0\n");
+            // a node cannot have more than 2 parents, invalid tree
+            std::cout << 0 << std::endl;
             return 0;
         }
-        if (tree[start].son[0] != -1)
-        {
-            tree[start].son[1] = stop;
-        }
-        else
-        {
-            tree[start].son[0] = stop;
-        }
-        tree[stop].parents.push_back(start);
-
-        c = getchar();
     }
+
     // needs to make to both vertices
-    go_up(vertice1, vertice2, tree);
-    go_up(vertice2, vertice1, tree);
+    go_up(node1_i, node2_i, tree);
+    go_up(node2_i, node1_i, tree);
 
     if (all_possible.empty())
-        printf("-");
+    {
+        std::cout << "-";
+    }
     else
     {
-        sort(all_possible.begin(), all_possible.end());
-
         for (int value : all_possible)
         {
-            printf("%d ", value);
+            std::cout << value << ' ';
         }
     }
-    printf("\n");
+    std::cout << std::endl;
 
     return 0;
 }
@@ -92,15 +80,16 @@ void go_up(int current, int target, node *all_nodes)
     for (int above : all_nodes[current].parents)
     {
         // add number to vector
-        if (go_down(current, above, target, all_nodes) == true)
+        if (go_down(current, above, target, all_nodes))
         {
-            all_possible.push_back(above);
+            all_possible.insert(above);
             // return because the above are not the closest
             return;
         }
         go_up(above, target, all_nodes);
     }
 }
+
 bool go_down(int original, int current, int target, node *all_nodes)
 {
     // it means that it went up and got down to the same node, it would give a bad output if not taken into consideration
@@ -114,11 +103,10 @@ bool go_down(int original, int current, int target, node *all_nodes)
     }
 
     // visit sons
-    for (int i = 0; i < NUM_CHILD; i++)
+    for (int child : all_nodes[current].children)
     {
-        if (all_nodes[current].son[i] != -1)
-            if (go_down(original, all_nodes[current].son[i], target, all_nodes) == true)
-                return true;
+        if (go_down(original, child, target, all_nodes))
+            return true;
     }
     return false;
 }
