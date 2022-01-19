90089ff07ebbd843e845612590506f5d09f96b54
Diogo Correia <me@diogotc.com>
2022-01-19 20:29:44 +0000

    Improve complexity to O(V + E) (#8)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <queue>
-#include <set>
 #include <string.h>
 
 #define NUM_PARENTS 2
@@ -8,6 +7,8 @@
 typedef struct
 {
     int parents[NUM_PARENTS];
+    int children_count;
+    short visited;
 } node_t;
 
 typedef enum color
@@ -17,18 +18,9 @@ typedef enum color
     BLACK
 } color_t;
 
-typedef struct
-{
-    color_t color;
-    int distance;
-    int pi;
-    node_t *node;
-} bfs_node;
-
 bool try_insert_array(int *arr, int element, size_t arr_size);
 
-std::set<int> *get_reachable_nodes(int start_node, int node_count, node_t *tree);
-void strip_parents(int start_node, std::set<int> *set, node_t *tree);
+void calculate_reachable_nodes(int start_node, int node_count, node_t *tree);
 bool has_loops(int node_count, node_t *tree);
 bool dfs_visit_loop_check(color_t *node_colors, node_t *tree, int current_node);
 
@@ -45,6 +37,8 @@ int main()
         for (int j = 0; j < NUM_PARENTS; ++j)
         {
             tree[i].parents[j] = -1;
+            tree[i].children_count = 0;
+            tree[i].visited = 0;
         }
     }
 
@@ -59,42 +53,47 @@ int main()
             std::cout << 0 << std::endl;
             return 0;
         }
+        tree[start - 1].children_count += 1;
     }
 
     if (has_loops(num_vertices, tree))
     {
+        // tree cannot have loops, invalid tree
         std::cout << 0 << std::endl;
         return 0;
     }
 
     // execute "reverse" BFS
-    std::set<int> *v1_parents = get_reachable_nodes(node1_i - 1, num_vertices, tree);
-    std::set<int> *v2_parents = get_reachable_nodes(node2_i - 1, num_vertices, tree);
-    std::set<int> common_parents;
+    calculate_reachable_nodes(node1_i - 1, num_vertices, tree);
+    calculate_reachable_nodes(node2_i - 1, num_vertices, tree);
 
-    // find the common parents
-    for (int el : *v1_parents)
+    // chop off non-common nodes from the tree
+    for (int el = 0; el < num_vertices; ++el)
     {
-        if (v2_parents->count(el) > 0)
+        if (tree[el].visited < 2)
         {
-            common_parents.insert(el);
+            for (int i = 0; i < NUM_PARENTS; ++i)
+            {
+                if (tree[el].parents[i] == -1)
+                    break;
+                tree[tree[el].parents[i]].children_count -= 1;
+            }
         }
     }
-    for (int el : common_parents)
-    {
-        strip_parents(el, &common_parents, tree);
-    }
 
-    if (common_parents.empty())
+    // print result
+    bool empty = true;
+    for (int i = 0; i < num_vertices; ++i)
     {
-        std::cout << "-";
-    }
-    else
-    {
-        for (int value : common_parents)
+        if (tree[i].visited >= 2 && tree[i].children_count == 0)
         {
-            std::cout << value + 1 << ' ';
+            std::cout << i + 1 << ' ';
+            empty = false;
         }
+    }
+    if (empty)
+    {
+        std::cout << '-';
     }
     std::cout << std::endl;
 
@@ -114,20 +113,16 @@ bool try_insert_array(int *arr, int element, size_t arr_size)
     return false;
 }
 
-std::set<int> *get_reachable_nodes(int start_node, int node_count, node_t *tree)
+void calculate_reachable_nodes(int start_node, int node_count, node_t *tree)
 {
-    bfs_node *nodes = (bfs_node *)malloc(node_count * sizeof(bfs_node));
+    color_t *node_colors = (color_t *)malloc(node_count * sizeof(color_t));
 
     // init bfs_nodes
     for (int i = 0; i < node_count; ++i)
     {
-        nodes[i].color = WHITE;
-        nodes[i].distance = -1;
-        nodes[i].pi = -1;
-        nodes[i].node = tree + i;
+        node_colors[i] = WHITE;
     }
-    nodes[start_node].color = GRAY;
-    nodes[start_node].distance = 0;
+    node_colors[start_node] = GRAY;
 
     // apply bfs_algorithm
     std::queue<int> queue;
@@ -138,46 +133,18 @@ std::set<int> *get_reachable_nodes(int start_node, int node_count, node_t *tree)
         int node_i = queue.front();
         queue.pop();
 
-        for (int parent : nodes[node_i].node->parents)
+        for (int parent : tree[node_i].parents)
         {
             if (parent == -1)
                 break;
-            if (nodes[parent].color == WHITE)
+            if (node_colors[parent] == WHITE)
             {
-                nodes[parent].color = GRAY;
-                nodes[parent].distance = nodes[node_i].distance + 1;
-                nodes[parent].pi = node_i;
+                node_colors[parent] = GRAY;
                 queue.push(parent);
             }
         }
-        nodes[node_i].color = BLACK;
-    }
-
-    std::set<int> *reached_nodes = new std::set<int>;
-
-    for (int i = 0; i < node_count; ++i)
-    {
-        if (nodes[i].distance >= 0)
-        {
-            reached_nodes->insert(i);
-        }
-    }
-
-    return reached_nodes;
-}
-
-void strip_parents(int start_node, std::set<int> *set, node_t *tree)
-{
-    for (int parent : tree[start_node].parents)
-    {
-        if (parent == -1)
-            break;
-        auto set_it = set->find(parent);
-        if (set_it != set->end())
-        {
-            set->erase(set_it);
-        }
-        strip_parents(parent, set, tree);
+        node_colors[node_i] = BLACK;
+        tree[node_i].visited += 1;
     }
 }
 
@@ -213,6 +180,7 @@ bool dfs_visit_loop_check(color_t *node_colors, node_t *tree, int current_node)
         {
             if (dfs_visit_loop_check(node_colors, tree, adj_i))
             {
+                // propagate result
                 return true;
             }
         }
